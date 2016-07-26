3836b5d2d6bbf79714174d281e1122bb3203e463
Andreas Hellesvik <andreashellesvik@hotmail.com>
2016-07-26 21:51:24 +0200

    Added layout for main activity

--- app/src/main/java/no/prisguide/tilbud/OffersActivity.java
+++ app/src/main/java/no/prisguide/tilbud/OffersActivity.java
@@ -1,13 +1,38 @@
 package no.prisguide.tilbud;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class OffersActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = OffersActivity.class.getSimpleName();
+
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offers);
+
+        // TODO check if user has internet access
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+
     }
 }
